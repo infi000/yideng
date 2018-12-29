5d995ff38b9d4a3fb04ab1461d5f9a7e4f9f119e
113079767@qq.com <113079767@qq.com>
2018-12-29 17:25:08 +0800

    1.初识PHP

---
+++ 06-07PHP与MySQL开发入门/1/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>1.初识PHP</title>
+</head>
+<body>
+    
+<?php
+//块级作用域
+// $a="测试";
+// function test() {
+//     echo $a;
+// }
+// test();
+// //报错！php函数内无法获取到全局变量（块级作用域）
+
+// function test() {
+//     global $a;
+//     echo $a;
+// }
+// test();
+// //成功 。需要引入全局变量$a
+// $a="测试";
+//     if(isset($b)){
+//         echo $a;
+//     }else{
+//         echo "a undefind ";
+//     }
+?>
+
+</body>
+</html>
